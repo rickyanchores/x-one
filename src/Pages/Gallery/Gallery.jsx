034bb1bb114e65de68c1ba4421e68366de9eb2cd
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-06-25 21:03:55 +0100

    edit Gallery grid

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -6,10 +6,18 @@ const Gallery = () => {
     <div className="Gallery flex h-auto p-10 justify-center items-center md:flex flex-col">
         <>Gallery</>
         <div className="gallery-container md:flex">
+          <div class="A">
             <Card />
+          </div>
+          <div class="B">
             <Card />
+          </div>
+          <div class="C">
             <Card />
+          </div>
+          <div class="D">
             <Card />
+          </div>
         </div>
     </div>
   )
