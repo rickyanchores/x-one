20408fcbc7ba8cef133cbf17d13aef0ef2973f49
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-06-25 21:15:30 +0100

    add GRID

---
+++ src/Components/Grid/Grid.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Card from '../Card/Card';
+
+const Grid = () => {
+  return (
+    <div className="Grid">
+        <div class="container">
+            <div class="A">
+                <Card />
+            </div>
+            <div class="B">
+                <Card />
+            </div>
+            <div class="C">
+                <Card />
+            </div>
+            <div class="D">
+                <Card />
+            </div>
+        </div>  
+    </div>
+  )
+}
+
+export default Grid;
--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Grid from '../../Components/Grid/Grid';
 
 const Services = () => {
   return (
-    <div className="Services h-screen flex justify-center items-center">Services</div>
+    <div className="Services h-auto bg-zinc-950 p-5 flex flex-col justify-center items-center">
+      <div className="title">Services</div>
+      <Grid />
+    </div>
   )
 }
 
