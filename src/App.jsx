b9c57867903f82f782f9f27a96e2b1453650f5d2
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-06-30 00:28:27 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Team from './Pages/Team/Team'
 
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import Hero from './Components/Hero/Hero'
 // ..
 AOS.init();
 
@@ -30,6 +31,7 @@ function App() {
           <Route component={Home}/>
         </Routes>
         <Home />
+        <Hero />
         <About />
         <Banner />
         <Gallery />
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="Hero bg-slate-700 p-20">
+        <h1>Hero</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In repellendus deserunt nobis ad magnam possimus.</p>
+    </div>
+  )
+}
+
+export default Hero;
