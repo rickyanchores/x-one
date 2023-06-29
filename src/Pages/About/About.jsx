5a92bdf58c50a32d66315cfe2822eb9a6f46a2c8
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-06-29 15:28:15 +0100

    added About section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="About bg-orange-600 p-64">
-        <div className="title">About</div>
-        <p>What is about???</p>
+    <div className="About bg-orange-500 p-20 w-2/3 m-auto mb-5 mt-5 rounded-xl">
+        <div className="title text-lg mb-5">
+          <h1>About</h1>
+        </div>
+        <p>Welcome to our web development portfolio! 
+
+At [Your Name/Company], we specialize in creating exceptional websites. Our team of skilled developers and designers is dedicated to crafting cutting-edge solutions tailored to your unique needs. From front-end to back-end development, e-commerce platforms to responsive design, we deliver top-notch results that leave a lasting impression. Explore our portfolio and let's create something amazing together. Contact us today!</p>
     </div>
   )
 }
