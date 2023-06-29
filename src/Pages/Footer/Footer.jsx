8287f39617e44ea0b7afad499e9f5303d63cdb8c
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-06-29 20:17:29 +0100

    added footer content

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="Footer h-screen flex justify-center items-center">Footer</div>
+    <div className="Footer h-screen flex justify-center items-center">
+      <div className="container">
+        <p>&copy; {new Date().getFullYear()} Your Website</p>
+      </div>
+    </div>
   )
 }
 
